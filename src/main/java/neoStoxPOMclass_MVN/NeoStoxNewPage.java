49df69478790a6c88075838b30678ffe50fcdecf
Rahul Joge <rahuljoge@gmail.com>
2023-02-03 11:54:31 +0530

    Added method enterMobileNumber in NeoStoxNewPage

--- src/main/java/neoStoxPOMclass_MVN/NeoStoxNewPage.java
+++ src/main/java/neoStoxPOMclass_MVN/NeoStoxNewPage.java
@@ -10,6 +10,7 @@ public class NeoStoxNewPage
 {
 	//1 Data member should be declared globally with access level private using @findBy Annotation
 			@FindBy(xpath = "(//a[text()='Sign In'])[1]") private WebElement signInButton;
+			@FindBy(id = "MainContent_signinsignup_txt_mobilenumber") private WebElement mobileNoButton;
 
 			//2 Initialize within a constructor with access level public using PageFactory
 			public NeoStoxNewPage (WebDriver driver)
@@ -22,6 +23,10 @@ public class NeoStoxNewPage
 			signInButton.click();
 			Reporter.log("clicking on signIn button", true);
 			}
-			
+			public void enterMobileNumber(String MobNum) 
+			{
+				mobileNoButton.sendKeys(MobNum);
+				Reporter.log("Entering mobile number", true);
+			}
 			
 }
